8f46c454230d0a1a3ebab19209c9ae1d314cc0b7
t0rchw00d <twilson170@gmail.com>
2019-10-23 15:54:26 +1100

    Working 4x4 Matric with RTS enabled and completed

--- MAT102 assignment 1 Tri3/MAT102 assignment 1 Tri3/Matrix.cpp
+++ MAT102 assignment 1 Tri3/MAT102 assignment 1 Tri3/Matrix.cpp
@@ -155,99 +155,163 @@ float CMatrix4::GetElement4(int _iX, int _iY)
 	return m_f4Matrix[_iX][_iY];
 }
 
-CMatrix3 & CMatrix4::Identity4(CMatrix3 & _rResult)
+CMatrix4 & CMatrix4::Identity4(CMatrix4 & _rResult)
 {
-	// TODO: insert return statement here
-}
-
-CMatrix3 & CMatrix4::Zero4(CMatrix3 & _rResult)
-{
+	_rResult.Zero4(_rResult);
 	for (int i = 0; i < 4; i++) {
-		for (int j = 0; j < 4; j++) {
-			_rResult.SetElement(i, j, 0);
-		}
+		_rResult.m_f4Matrix[i][i] = 1;
 	}
 	return _rResult;
 	// TODO: insert return statement here
 }
 
-bool CMatrix4::Equals4(CMatrix3 & _rA, CMatrix3 & _rB)
+CMatrix4 & CMatrix4::Zero4(CMatrix4 & _rResult)
 {
 	for (int i = 0; i < 4; i++) {
 		for (int j = 0; j < 4; j++) {
-			if (_rA.GetElement(i, j) != (_rB.GetElement(i, j))) {
+			_rResult.SetElement4(i, j, 0);
+		}
+	}
+	return _rResult;
+	//TODO: write test
+}
+
+bool CMatrix4::Equals4(CMatrix4 & _rA, CMatrix4 & _rB)
+{
+	for (int i = 0; i < 4; i++) {
+		for (int j = 0; j < 4; j++) {
+			if (_rA.GetElement4(i, j) != (_rB.GetElement4(i, j))) {
 				return false;
 			}
 		}
 	}
 	return true;
+	//TODO: write test
 }
 
-CMatrix3 & CMatrix4::Add4(CMatrix3 & _rA, CMatrix3 & _rB, CMatrix3 & _rResult)
+CMatrix4 & CMatrix4::Add4(CMatrix4 & _rA, CMatrix4 & _rB, CMatrix4 & _rResult)
 {
 	for (int i = 0; i < 4; i++) {
 		for (int j = 0; j < 4; j++) {
-			_rResult.SetElement(i,j, (_rA.GetElement(i, j) + _rB.GetElement(i, j)));
+			_rResult.SetElement4(i, j, (_rA.GetElement4(i, j) + _rB.GetElement4(i, j)));
 		}
 	}
 
 	return _rResult;
-
-	// TODO: insert return statement here
+	//TODO: wite test
 }
 
-CMatrix3 & CMatrix4::Subtract4(CMatrix3 & _rA, CMatrix3 & _rB, CMatrix3 & _rResult)
+CMatrix4 & CMatrix4::Subtract4(CMatrix4 & _rA, CMatrix4 & _rB, CMatrix4 & _rResult)
 {
 	for (int i = 0; i < 4; i++) {
 		for (int j = 0; j < 4; j++) {
-			_rResult.SetElement(i, j, (_rA.GetElement(i, j) - _rB.GetElement(i, j)));
+			_rResult.SetElement4(i, j, (_rA.GetElement4(i, j) - _rB.GetElement4(i, j)));
 		}
 	}
 	return _rResult;
-	// TODO: insert return statement here
+
+	// TODO: write test
 }
 
-CMatrix3 & CMatrix4::Multiply4(float _fScalar, CMatrix3 & _rA, CMatrix3 & _rResult)
+CMatrix4 & CMatrix4::Multiply4(float _fScalar, CMatrix4 & _rA, CMatrix4 & _rResult)
 {
 	for (int i = 0; i < 4; i++) {
 		for (int j = 0; j < 3; j++) {
-			_rResult.SetElement(i,j,(_rA.GetElement(i,j) * _fScalar));
+			_rResult.SetElement4(i, j, (_rA.GetElement4(i, j) * _fScalar));
 		}
 	}
 	return _rResult;
-	// TODO: insert return statement here
+
+	// TODO: write test
 }
 
-CMatrix3 & CMatrix4::Multiply4(CMatrix3 & _rA, CMatrix3 & _rB, CMatrix3 & _rResult)
+CMatrix4 & CMatrix4::Multiply4(CMatrix4 & _rA, CMatrix4 & _rB, CMatrix4 & _rResult)
 {
 	for (int i = 0; i < 4; i++) {
 		for (int j = 0; j < 4; j++) {
-			_rResult.SetElement(i,j, 0.0f);
+			_rResult.SetElement4(i, j, 0.0f);
 			for (int k = 0; k < 3; k++) {
-				_rResult.SetElement(i,j,(_rResult.GetElement(i,j) + _rA.GetElement(i,k) * _rB.GetElement(k,j)));
+				_rResult.SetElement4(i, j, (_rResult.GetElement4(i, j) + _rA.GetElement4(i, k) * _rB.GetElement4(k, j)));
 			}
 		}
 	}
 	return _rResult;
-	// TODO: insert return statement here
+
+	// TODO: write test
 }
 
-CMatrix3 & CMatrix4::Transpose4(CMatrix3 & _rA, CMatrix3 & _rResult)
+CMatrix4 & CMatrix4::Transpose4(CMatrix4 & _rA, CMatrix4 & _rResult)
 {
 	for (int i = 0; i < 4; i++) {
 		for (int j = 0; j < 4; j++) {
-			_rResult.SetElement(i,j, _rA.GetElement(j,i));
+			_rResult.SetElement4(i, j, _rA.GetElement4(j, i));
 		}
 	}
+	return _rResult;
 	// TODO: insert return statement here
 }
 
-float CMatrix4::Detriminant4(CMatrix3 & _rA)
+float CMatrix4::Detriminant4(CMatrix4 & _rA)
 {
 	return 0.0f;
 }
 
-CMatrix3 & CMatrix4::Inverse4(CMatrix3 & _rA, CMatrix3 & _rResult)
+CMatrix4 & CMatrix4::Inverse4(CMatrix4 & _rA, CMatrix4 & _rResult)
 {
 	// TODO: insert return statement here
+	return _rResult;
 }
+
+CMatrix4 & CMatrix4::Translate(CMatrix4 & _rA, CMatrix4 & _rResult, int xTranslate, int yTranslate, int zTranslate)
+{
+	_rResult.SetElement4(0, 3, (_rA.GetElement4(0, 3) + xTranslate));
+	_rResult.SetElement4(1, 3, (_rA.GetElement4(1, 3) + yTranslate));
+	_rResult.SetElement4(2, 3, (_rA.GetElement4(2, 3) + zTranslate));
+
+	return _rResult;
+}
+
+CMatrix4 & CMatrix4::Scaling(CMatrix4 & _rA, CMatrix4 & _rResult, int scale)
+{
+	_rResult.SetElement4(0, 0, (_rA.GetElement4(0, 0) * scale));
+	_rResult.SetElement4(1, 1, (_rA.GetElement4(1, 1) * scale));
+	_rResult.SetElement4(2, 2, (_rA.GetElement4(2, 2) * scale));
+	return _rResult;
+	// TODO: insert return statement here
+}
+
+CMatrix4 & CMatrix4::RotateX(CMatrix4 & _rA, CMatrix4 & _rResult, int rAngle)
+{
+	_rResult.SetElement4(1, 1, _rA.GetElement4(1, 1) + cos(rAngle));
+	_rResult.SetElement4(1, 2, _rA.GetElement4(1, 2) + sin(rAngle));
+	_rResult.SetElement4(2, 1, _rA.GetElement4(2, 1) + sin(rAngle));
+	_rResult.SetElement4(2, 2, _rA.GetElement4(2, 2) + cos(rAngle));
+
+	return _rResult;
+
+	// TODO: insert return statement here
+}
+
+CMatrix4 & CMatrix4::RotateY(CMatrix4 & _rA, CMatrix4 & _rResult, int rAngle)
+{
+	_rResult.SetElement4(0, 0, _rA.GetElement4(0, 0) + cos(rAngle));
+	_rResult.SetElement4(2, 0, _rA.GetElement4(2, 0) + sin(rAngle));
+	_rResult.SetElement4(0, 2, _rA.GetElement4(0, 2) + sin(rAngle));
+	_rResult.SetElement4(2, 2, _rA.GetElement4(2, 2) + cos(rAngle));
+
+	return _rResult;
+
+
+	// TODO: insert return statement here
+}
+
+CMatrix4 & CMatrix4::RotateZ(CMatrix4 & _rA, CMatrix4 & _rResult, int rAngle)
+{
+
+	_rResult.SetElement4(0, 0, _rA.GetElement4(0, 0) + cos(rAngle));
+	_rResult.SetElement4(0, 1, _rA.GetElement4(0, 1) + cos(rAngle));
+	_rResult.SetElement4(1, 0, _rA.GetElement4(1, 0) + cos(rAngle));
+	_rResult.SetElement4(1, 1, _rA.GetElement4(1, 1) + cos(rAngle));
+	// TODO: insert return statement here
+}
+
--- MAT102 assignment 1 Tri3/MAT102 assignment 1 Tri3/Matrix.h
+++ MAT102 assignment 1 Tri3/MAT102 assignment 1 Tri3/Matrix.h
@@ -33,15 +33,20 @@ public:
 
 	float SetElement4(int _iX, int _iY, float _fValue);
 	float GetElement4(int _iX, int _iY);
-	static CMatrix3& Identity4(CMatrix3& _rResult);
-	static CMatrix3& Zero4(CMatrix3& _rResult);
-	static bool Equals4(CMatrix3& _rA, CMatrix3& _rB);
-	static CMatrix3& Add4(CMatrix3& _rA, CMatrix3& _rB, CMatrix3& _rResult);
-	static CMatrix3& Subtract4(CMatrix3& _rA, CMatrix3& _rB, CMatrix3& _rResult);
-	static CMatrix3& Multiply4(float _fScalar, CMatrix3& _rA, CMatrix3& _rResult);
-	static CMatrix3& Multiply4(CMatrix3& _rA, CMatrix3& _rB, CMatrix3& _rResult);
-	static CMatrix3& Transpose4(CMatrix3& _rA, CMatrix3& _rResult);
-	static float Detriminant4(CMatrix3& _rA);
-	static CMatrix3& Inverse4(CMatrix3& _rA, CMatrix3& _rResult);
+	static CMatrix4& Identity4(CMatrix4& _rResult);
+	static CMatrix4& Zero4(CMatrix4& _rResult);
+	static bool Equals4(CMatrix4& _rA, CMatrix4& _rB);
+	static CMatrix4& Add4(CMatrix4& _rA, CMatrix4& _rB, CMatrix4& _rResult);
+	static CMatrix4& Subtract4(CMatrix4& _rA, CMatrix4& _rB, CMatrix4& _rResult);
+	static CMatrix4& Multiply4(float _fScalar, CMatrix4& _rA, CMatrix4& _rResult);
+	static CMatrix4& Multiply4(CMatrix4& _rA, CMatrix4& _rB, CMatrix4& _rResult);
+	static CMatrix4& Transpose4(CMatrix4& _rA, CMatrix4& _rResult);
+	static float Detriminant4(CMatrix4& _rA);
+	static CMatrix4& Inverse4(CMatrix4& _rA, CMatrix4& _rResult);
 
+	static CMatrix4& Translate(CMatrix4 & _rA, CMatrix4& _rResult, int xTranslate, int yTranslate, int zTranslate);
+	static CMatrix4& Scaling(CMatrix4& _rA, CMatrix4 & _rResult, int scale);
+	static CMatrix4& RotateX(CMatrix4& _rA, CMatrix4& _rResult, int rAngle);
+	static CMatrix4& RotateY(CMatrix4& _rA, CMatrix4& _rResult, int rAngle);
+	static CMatrix4& RotateZ(CMatrix4& _rA, CMatrix4& _rResult, int rAngle);
 };
